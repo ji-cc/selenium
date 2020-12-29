d7301af8e536065a5577566daa2e14daedb76adb
ji-cc <jicc0610@163.com>
2020-12-29 22:04:01 +0800

    提交

--- test_1223.py
+++ test_1223.py
@@ -13,7 +13,7 @@ time.sleep(4)
 
 # # name的定位   不是每个元素都有name属性
 # driver.find_element_by_name("wd").send_keys("yamy")
-driver.find_element_by_id("su").click()   # “百度一下” 的按钮没有name属性，所有还是用id定位
+# driver.find_element_by_id("su").click()   # “百度一下” 的按钮没有name属性，所有还是用id定位
 
 # class name的定位   # 必须保证class name 的属性只有一个并且唯一，才能用class name定位
 # driver.find_element_by_class_name("s_ipt").send_keys("java")
@@ -38,14 +38,27 @@ driver.find_element_by_id("su").click()   # “百度一下” 的按钮没有na
 # driver.find_element_by_css_selector("#kw").send_keys("雪橇三傻")
 # driver.find_element_by_xpath("//*[@id='su']").click()
 
-
-driver.find_element_by_id("kw").send_keys("三十而已")
-driver.find_element_by_id("su").click()
-time.sleep(2)
+# driver.find_element_by_id("kw").send_keys("三十而已")
+# driver.find_element_by_id("su").click()
+# time.sleep(2)
 # 清除百度搜索框的内容
-driver.find_element_by_id("kw").clear()
-driver.find_element_by_id("kw").send_keys("四十不惑")
-driver.find_element_by_id("su").click()
+# driver.find_element_by_id("kw").clear()
+# driver.find_element_by_id("kw").send_keys("四十不惑")
+# driver.find_element_by_id("su").click()
+
+# submit 提交 == 点击
+driver.find_element_by_id("kw").send_keys("乃万")
+driver.find_element_by_id("su").submit()
+# 设置等待时间
+# 固定等待时间
+time.sleep(10)
+
+driver.find_element_by_partial_link_text("乃万_百度百科").click()
+
+# text 获取文本内容
+# text = driver.find_element_by_id("s-bottom-layer-right").text
+# print(text)
+
 
 
 
