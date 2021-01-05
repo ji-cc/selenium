966973f49f31b454db1301f832cb2487954ac2ad
ji-cc <jicc0610@163.com>
2021-01-05 21:43:44 +0800

    提交

--- 01.04.py
+++ 01.04.py
@@ -1,6 +1,6 @@
-from selenium import  webdriver
+from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import  Keys  #需要引入keys包
+from selenium.webdriver.common.keys import Keys  #需要引入keys包
 
 # 键盘事件
 driver = webdriver.Firefox()   #打开浏览器
--- test_1223.py
+++ test_1223.py
@@ -1,5 +1,7 @@
 from selenium import  webdriver
 import time
+from selenium.webdriver.common.keys import Keys  # 需要引入keys包
+from selenium.webdriver.common.action_chains import ActionChains   # 引入键盘事件所需要的包
 
 driver = webdriver.Firefox()   #打开浏览器
 driver.get("https://www.baidu.com/")  # 百度网页
@@ -10,8 +12,8 @@ driver.get("https://www.baidu.com/")  # 百度网页
 # driver.minimize_window()
 
 # 设置浏览器的宽和高
-driver.set_window_size(400, 800)
-time.sleep(4)
+# driver.set_window_size(400, 800)
+# time.sleep(4)
 
 # # id 的定位
 # driver.find_element_by_id("kw").send_keys("三十而已")  #  在百度网页右键检查百度搜索框的id是“kw”, 搜索"三十而已"
@@ -54,28 +56,57 @@ time.sleep(4)
 
 # submit 提交 == 点击
 driver.find_element_by_id("kw").send_keys("乃万")
-driver.find_element_by_id("su").submit()
+# driver.find_element_by_id("su").submit()
+time.sleep(4)
 # 设置等待时间
 # 固定等待时间
 # time.sleep(10)
 # 智能等待时间，页面加载完成后，不需要固定等待10s，可以立即点击
-driver.implicitly_wait(20)
-driver.find_element_by_link_text("乃万(豆瓣)").click()
+# driver.implicitly_wait(20)
+# driver.find_element_by_link_text("乃万(豆瓣)").click()
 
 # 打印信息(只能打印前一个网页的title和URL)
 # 打印title url
-title = driver.title
-print("title = "+title)
-url = driver.current_url
-print("URL = "+url)
-time.sleep(6)
-# 浏览器的滚动条拖动到最低端
-js = "var q=document.documentElement.scrollTop=100000"
-driver.execute_script(js)
-time.sleep(6)
-# 浏览器的滚动条推动到最顶端
-js0 = "var q=document.documentElement.scrollTop=0"
-driver.execute_script(js0)
+# title = driver.title
+# print("title = "+title)
+# url = driver.current_url
+# print("URL = "+url)
+# time.sleep(6)
+# # 浏览器的滚动条拖动到最低端
+# js = "var q=document.documentElement.scrollTop=100000"
+# driver.execute_script(js)
+# time.sleep(6)
+# # 浏览器的滚动条推动到最顶端
+# js0 = "var q=document.documentElement.scrollTop=0"
+# driver.execute_script(js0)
+
+# 组合键盘事件
+# 复制 ctrl+a      先定位到要复制的输入框，再发送组合键
+driver.find_element_by_id("kw").send_keys(Keys.CONTROL, 'a')
+time.sleep(4)
+# 剪贴   ctrl+x
+driver.find_element_by_id("kw").send_keys(Keys.CONTROL, 'x')
+time.sleep(4)
+
+driver.find_element_by_id("kw").send_keys("赵丽颖")
+# 定位到“百度一下”按钮
+su1 = driver.find_element_by_id("su")
+# 右击
+ActionChains(driver).context_click(su1).perform()
+# 双击
+time.sleep(5)
+ActionChains(driver).double_click(su1).perform()
+
+time.sleep(5)
+title = driver.find_element_by_id("su")
+target = driver.find_element_by_link_text("赵丽颖-百度百科 ")
+# 拖动  从“百度一下”拖动到“百度百科”
+ActionChains(driver).drag_and_drop(title, target).perform()
+
+# 移动 把鼠标移动到目标元素上
+ActionChains(driver).move_to_element(target).perform()
+
+
 
 # text 获取文本内容
 # text = driver.find_element_by_id("s-bottom-layer-right").text
